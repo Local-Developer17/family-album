29428313a5bf3733ea7a3c006c2dd9a1131b57d8
Local-Developer17 <150911811+Local-Developer17@users.noreply.github.com>
2024-06-02 22:06:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const sidebarElem = document.querySelector(".sidebar");
 const dropdownElem = document.querySelector(".dropdown");
 const galleryElem = document.querySelector(".gallery-container");
+const galleryElemAll = document.querySelector(".gallery-container");
 const container = document.querySelector(".gallery");
 const headingElem = document.querySelector(".gallery-heading");
 const image = document.getElementsByTagName("img");
@@ -12,15 +13,6 @@ const slidesEl = document.querySelector(".slides");
 const sloganEl = document.querySelector(".slogan");
 const titleEl = document.querySelector(".title");
 const plansEl = document.querySelector(".plans");
-const allImages = [
-  ["images/q1/pic-1.jpg", "images/q1/pic-2.jpg", "images/q1/pic-3.jpg"],
-  ["images/q2/pic-1.jpg", "images/q2/pic-2.jpg", "images/q2/pic-3.jpg"],
-  ["images/q3/pic-1.jpg", "images/q3/pic-2.jpg", "images/q3/pic-3.jpg"],
-  ["images/q4/pic-1.jpg", "images/q4/pic-2.jpg", "images/q4/pic-3.jpg"],
-];
-const slideImages = [...allImages[0]];
-const slideImages2 = [...allImages[1]];
-const slideImages3 = [...allImages[2], ...allImages[3]];
 const RightToggleUrl = "images/toggle-right.png";
 const leftToggleUrl = "images/toggle-left.png";
 const closeBtnUrl = "images/cancel.png";
@@ -29,6 +21,18 @@ const buyBtn = document.getElementsByTagName("button");
 const firstSlide = document.getElementById("first");
 const secondSlide = document.getElementById("second");
 const thirdSlide = document.getElementById("third");
+const overlay = document.querySelector(".overlay");
+const preview = document.querySelector(".preview");
+const allImages = [
+  ["images/q1/pic-1.jpg", "images/q1/pic-2.jpg", "images/q1/pic-3.jpg"],
+  ["images/q2/pic-1.jpg", "images/q2/pic-2.jpg", "images/q2/pic-3.jpg"],
+  ["images/q3/pic-1.jpg", "images/q3/pic-2.jpg", "images/q3/pic-3.jpg"],
+  ["images/q4/pic-1.jpg", "images/q4/pic-2.jpg", "images/q4/pic-3.jpg"],
+];
+const slideImages = [...allImages[0], ...allImages[1], ...allImages[2], ...allImages[3]];
+const slideImages2 = [...allImages[0], ...allImages[1], ...allImages[2], ...allImages[3]];
+const slideImages3 = [...allImages[0], ...allImages[1], ...allImages[2], ...allImages[3]];
+const previewAll = document.querySelectorAll(".previewImg");
 let isZoomed = "";
 let pictureCount = 0;
 let counter = 0;
@@ -40,6 +44,7 @@ sidebarElem.addEventListener("mouseover", () => {
 sidebarElem.addEventListener("mouseleave", () => {
   dropdownElem.classList.add("hidden");
 });
+//Plan Satın Alma(Price Cards):
 buyBtn[1].onclick = () => {
   document.querySelector(".premium").classList.add("selected");
   document.querySelector(".ultimate").classList.remove("selected");
@@ -65,26 +70,48 @@ document.querySelector(".q4").onclick = () => {
   switchQuarter(3);
   refreshGallery(2);
 };
+let previewImg;
 //Galeride Resimleri Tıklanan çeyreğe göre dizme:
 function switchQuarter(input) {
   headingEl.classList.remove("hidden");
   container.classList.remove("hidden");
   slidesEl.classList.add("hidden");
   galleryElem.innerHTML = "";
-  for (let [index, value] of allImages[input].entries()) {
+  allImages[input].forEach((value, index) => {
     let newImg = document.createElement("img");
     newImg.setAttribute("src", value);
     galleryElem.appendChild(newImg);
     //Resmi büyüt:
     image[index + 5].onclick = () => {
+      allImages[input].forEach((value, i) => {
+        //Preview Componentindeki İmajların gelmesi:
+        previewImg = document.createElement("img");
+        previewImg.setAttribute("src", value);
+        previewImg.classList.add("previewImg");
+        preview.classList.remove("hidden");
+        preview.appendChild(previewImg);
+      });
+      //Büyütülen resmin preview seçili görünmesi:
+      document.querySelectorAll(".previewImg")[index].classList.add("current");
       pic.src = value;
       openImage();
       toggleBtn(index, allImages[input].length - 1, input);
       closeBtn();
       picIndicator(index + 1, allImages[input].length);
+      //Preview'den seçilen fotoğrafı zoom componentine getirme:
+      document.querySelectorAll(".previewImg").forEach((value, index) => {
+        value.onclick = () => {
+          //Preview'deki tüm fotoğrafların gölgesini kaldırıp, gölgeyi seçili olana ekleme:
+          document.querySelectorAll(".previewImg").forEach((pic) => pic.classList.remove("current"));
+          pic.src = value.src;
+          value.classList.add("current");
+          toggleBtn(index, allImages[input].length - 1, input);
+        };
+      });
     };
-  }
+  });
 }
+
 //Büyütülen Resimler Arası Geçiş tuşları:
 const RightToggle = document.createElement("img");
 const leftToggle = document.createElement("img");
@@ -95,12 +122,16 @@ function toggleBtn(counter, pictureCount, input) {
   container.appendChild(RightToggle);
   RightToggle.onclick = () => {
     if (counter >= pictureCount) {
+      document.querySelectorAll(".previewImg")[counter].classList.remove("current");
       counter = 0;
       pic.src = allImages[input][counter];
+      document.querySelectorAll(".previewImg")[counter].classList.add("current");
       swipeRight();
       indicator.textContent = `${counter + 1} / ${pictureCount + 1}`;
     } else {
       pic.src = allImages[input][counter + 1];
+      document.querySelectorAll(".previewImg")[counter].classList.remove("current");
+      document.querySelectorAll(".previewImg")[counter + 1].classList.add("current");
       counter++;
       swipeRight();
       indicator.textContent = `${counter + 1} / ${pictureCount + 1}`;
@@ -112,12 +143,16 @@ function toggleBtn(counter, pictureCount, input) {
   container.appendChild(leftToggle);
   leftToggle.onclick = () => {
     if (counter < 1) {
+      document.querySelectorAll(".previewImg")[counter].classList.remove("current");
       counter = pictureCount;
       pic.src = allImages[input][counter];
+      document.querySelectorAll(".previewImg")[counter].classList.add("current");
       swipeLeft();
       indicator.textContent = `${counter + 1} / ${pictureCount + 1}`;
     } else {
       pic.src = allImages[input][counter - 1];
+      document.querySelectorAll(".previewImg")[counter].classList.remove("current");
+      document.querySelectorAll(".previewImg")[counter - 1].classList.add("current");
       counter--;
       swipeLeft();
       indicator.textContent = `${counter + 1} / ${pictureCount + 1}`;
@@ -152,25 +187,24 @@ function refreshGallery(input) {
   switch (input) {
     //Resmi kapatma:
     case 1:
-      //Animasyon Class'ı:
-      pic.classList.add("zoomout");
+      //Animasyon Class'ı Silme:
       container.removeChild(closeImg);
       container.removeChild(RightToggle);
       container.removeChild(leftToggle);
       container.removeChild(indicator);
       main.classList.remove("fix-height");
-      setTimeout(() => {
-        container.classList.remove("overlay");
-        pic.classList.add("hidden");
-        galleryElem.classList.remove("hidden");
-        headingEl.classList.remove("hidden");
-        pic.classList.remove("no-click");
-        pic.classList.remove("swipe-right");
-        welcomeEl.classList.remove("hidden");
-        pic.classList.remove("default");
-        pic.classList.remove("zoomout");
-        pic.classList.remove("zoom");
-      }, 200);
+      document.querySelectorAll(".previewImg").forEach((pic) => preview.removeChild(pic));
+      //Timeout ile Animasyon buraya eklenecek: SetTimeout(()=> {},timeout)
+      container.classList.remove("overlay");
+      pic.classList.add("hidden");
+      galleryElem.classList.remove("hidden");
+      headingEl.classList.remove("hidden");
+      pic.classList.remove("no-click");
+      pic.classList.remove("swipe-right");
+      welcomeEl.classList.remove("hidden");
+      pic.classList.remove("default");
+      pic.classList.remove("zoom");
+      preview.classList.add("hidden");
 
       break;
     //Galeriyi Değiştirme
@@ -187,7 +221,6 @@ document.addEventListener("keydown", (e) => {
     refreshGallery(1);
   }
 });
-
 //Animasyonlar:
 function swipeRight() {
   pic.classList.add("swipe-right");
@@ -205,22 +238,15 @@ function swipeLeft() {
   pic.classList.remove("zoom");
   pic.classList.add("default");
 }
+//Ana Menüdeki Slayt Resimleri:
 function slideAnimation() {
-  let counter = Math.floor(Math.random() * slideImages.length);
-  let counter2 = Math.floor(Math.random() * slideImages.length);
-  let counter3 = Math.floor(Math.random() * slideImages.length);
-  firstSlide.src = slideImages[counter];
-  secondSlide.src = slideImages2[counter2];
-  thirdSlide.src = slideImages3[counter3];
-  slideImages.forEach(() => {
-    setInterval(() => {
-      let counter = Math.floor(Math.random() * slideImages.length);
-      let counter2 = Math.floor(Math.random() * slideImages2.length);
-      let counter3 = Math.floor(Math.random() * slideImages3.length);
-      firstSlide.src = slideImages[counter];
-      secondSlide.src = slideImages2[counter2];
-      thirdSlide.src = slideImages3[counter3];
-    }, 4000);
-  });
+  firstSlide.src = slideImages[Math.floor(Math.random() * slideImages.length)];
+  secondSlide.src = slideImages2[Math.floor(Math.random() * slideImages2.length)];
+  thirdSlide.src = slideImages3[Math.floor(Math.random() * slideImages3.length)];
+  setInterval(() => {
+    firstSlide.src = slideImages[Math.floor(Math.random() * slideImages.length)];
+    secondSlide.src = slideImages2[Math.floor(Math.random() * slideImages2.length)];
+    thirdSlide.src = slideImages3[Math.floor(Math.random() * slideImages3.length)];
+  }, 4000);
 }
 slideAnimation();
